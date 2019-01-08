f85c1c2fca4b03e75bfea279fc55d67859871456
RafaeldeLaPuebla <46501857+RafaeldeLaPuebla@users.noreply.github.com>
2019-01-08 22:02:33 +0100

    Add files via upload

---
+++ cálculo media.py
@@ -0,0 +1,13 @@
+#Función que calcule la media de una lista de números.
+
+def esMedia(lista):
+    suma = 0
+    for n in lista:
+        suma +=n
+    return suma/len(lista)
+
+def calculaMedia(*items):
+    suma = 0
+    for n in items:
+        suma += n
+    return suma/len(items)
